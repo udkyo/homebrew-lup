3fa464dd4a55542db193df2a57b9031af875c29c
udkyo <discohammock@udk.io>
2018-07-24 23:33:49 +0100

    Brew formula update for lup version v0.4.0

--- Formula/lup.rb
+++ Formula/lup.rb
@@ -1,9 +1,9 @@
 class Lup < Formula
   desc "Loopy command execution."
   homepage "https://github.com/udkyo/lup/"
-  url "https://github.com/udkyo/lup/releases/download/v0.3.2/lup_0.3.2_darwin_amd64.tar.gz"
-  version "0.3.2"
-  sha256 "7dd3c12254a395d34a98e9f244eaa60c7a5a19847aeb799a6a4c9e9d065a04b9"
+  url "https://github.com/udkyo/lup/releases/download/v0.4.0/lup_0.4.0_darwin_amd64.tar.gz"
+  version "0.4.0"
+  sha256 "87af50a287f0b0e591036b799ec70f86c531f9e70f95f4c9baf1a0f6dec1753a"
 
   def install
     bin.install "lup"
